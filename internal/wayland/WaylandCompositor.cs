0859a432d5e6633f0f9bb9947ed11dff9718f0a8
Xtarii <carlalvinelias@gmail.com>
2026-06-01 14:54:46 +0200

    Wrapper for Wayland Compositor

---
+++ internal/wayland/WaylandCompositor.cs
@@ -0,0 +1,28 @@
+using System.Runtime.InteropServices;
+
+namespace Sharpland;
+
+/// <summary>
+/// Wayland compositor object wrapper
+/// </summary>
+internal class WaylandCompositor {
+    /// <summary>
+    /// Wayland compositor instance
+    /// </summary>
+    internal IntPtr Instance { get; private set; }
+
+
+
+    /// <summary>
+    /// Creates Wayland compositor object
+    /// </summary>
+    /// <param name="registry">Wayland registry object</param>
+    /// <param name="name">Compositor interface name</param>
+    /// <param name="version">Compositor interface version</param>
+    internal WaylandCompositor(WaylandRegistry registry, uint name, uint version) {
+        IntPtr @interface = WaylandInterface.Compositor();
+        Instance = registry.Bind(@interface, name, version);
+        if(Instance == IntPtr.Zero)
+            throw new ExternalException("Failed to create Wayland compositor.");
+    }
+}
